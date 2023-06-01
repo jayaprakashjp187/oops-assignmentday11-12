e760454983d24b6fed6ee5f489b6505a1ea71017
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-01 10:50:05 +0530

    find unique numbers

--- Day 11&12/Program.cs
+++ Day 11&12/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Day_11_12
+﻿using System.Data;
+
+namespace Day_11_12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Duplicate Program");
+            Console.WriteLine("1.Duplicate Program\n2.UniqueElements");
             Console.WriteLine("please choose what program u want");
             int choose =Convert.ToInt32(Console.ReadLine());
             switch(choose)
@@ -12,6 +14,10 @@
                 case 1:
                     Console.WriteLine("welcome to the duplicate program");
                     Duplicate.FindingDuplicateNumber();
+                    break;
+                case 2:
+                    Console.WriteLine("welcome to the unique elements program");
+                    Unique.FindingUniqueNumbers();
                     break;
             }
         }
---
+++ Day 11&12/Unique.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11_12
+{
+    internal class Unique
+    {
+        public static void FindingUniqueNumbers()
+        {
+            {
+                int[] numbers = { 1, 2, 3, 4, 5, 1, 2, 3, 6, 7 };
+
+                int[] uniqueNumbers = numbers.Distinct().ToArray();
+
+                Console.WriteLine("Unique numbers:");
+                foreach (int num in uniqueNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+        }
+    }
+}
