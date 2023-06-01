9c630d4baf01f8ce9eb7753800bcd021520cdf33
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-01 10:12:01 +0530

    counting of duplicate numbers

---
+++ Day 11&12/Duplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11_12
+{
+    internal class Duplicate
+    {
+        public static void FindingDuplicateNumber()
+        {
+            int[] duplicate = { 1, 2, 3, 4,  4, 5, 3, 5, 7, 9, 6, 7 };
+            int count = 0;
+          
+            for(int i=0; i<duplicate.Length; i++)
+            {
+                for (int j=i+1; j<duplicate.Length; j++)
+                {
+                    if (duplicate[j] == duplicate[i])
+                    {
+                        count++;
+                    }
+                  
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Day 11&12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day_11_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Duplicate Program");
+            Console.WriteLine("please choose what program u want");
+            int choose =Convert.ToInt32(Console.ReadLine());
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("welcome to the duplicate program");
+                    Duplicate.FindingDuplicateNumber();
+                    break;
+            }
+        }
+    }
+}
